375db04b39c1ca7dbccd6013d9188c281cad3a24
Ali202H <134901004+Ali202H@users.noreply.github.com>
2024-08-10 02:55:46 +0330

    Create Ali.sh

---
+++ Ali.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Print a message to the console
+echo "Hello, World!"
+
+# Create a new directory
+mkdir mydirectory
+
+# Create a new file
+touch myfile.txt
+
+# Write some text to the file
+echo "This is a test file" >> myfile.txt
+
+# Print the contents of the file
+cat myfile.txt
